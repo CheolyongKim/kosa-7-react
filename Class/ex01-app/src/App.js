91240478ec6b95af76108524f9facd9da7714444
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-07-13 17:07:35 +0900

    chore: today class done

---
+++ Class/ex01-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './component/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      my web site<br/>
+      <MyComponent/>
+      <MyComponent/>
+      <MyComponent/>
+      <MyComponent/>
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
